33abdffb2a8012b54ec70fc208816d9e449481c2
adriancuria <curia.gheorghe@yahoo.com>
2017-12-16 20:50:57 +0200

    commit 1

---
+++ src/main/java/com/dbbyte/sample/SbSecurityMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityMysqlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dbbyte/sample/SbSecurityMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbbyte.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbSecurityMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
